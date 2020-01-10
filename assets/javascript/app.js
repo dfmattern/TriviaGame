f51b5cb4551c47848515a4a81cf8575bc2ecd233
Dane <56905214+dfmattern@users.noreply.github.com>
2020-01-09 20:44:10 -0700

    created html, css, js scaffold, added background

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+$(function(){
+
+    let questionBank = [
+
+        {
+             question: "What color are aircraft black boxes?",
+            answerChoices: ["BLack", "Orange", "Red", "Gray"],
+         correctAnswer: 1,
+    },
+    {
+        question:
+        answerChoices:
+        correctAnswer:
+    }
+]
+    
+})
